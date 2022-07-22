ca5c78977e34185aeaf591b0373b2f91bbcea883
gessica da silva <gessicahugendobler@gmail.com>
2022-07-22 13:27:13 -0300

    has secure token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
   validates :password, presence: true
