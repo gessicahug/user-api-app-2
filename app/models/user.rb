bccbb7877b0ec090415dfe8b30d80002fa7109c5
gessica da silva <gessicahugendobler@gmail.com>
2022-07-22 13:15:54 -0300

    user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
-  validates :password, :name, presence: true
+  validates :password, presence: true
 end
